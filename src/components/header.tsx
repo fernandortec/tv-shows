f69dfb00c1cc13044d0fe6098713f24feeb36460
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-04-17 20:20:35 -0300

    :sparkles: (navbar) add styled navbar and logo in hedaer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,19 @@
 import styles from "./header.module.css";
-import logo from "../../public/tv.svg"
+import logo from "../../public/tv-white.svg";
+import { Navbar } from "./navbar";
 
 export function Header(): JSX.Element {
 	return (
 		<header className={styles.header}>
-			<div>
+			<div className={styles.logo}>
 				<img alt="" src={logo} className={styles.logo} />
 				<p>TvShows</p>
+			</div>
+
+			<Navbar />
+
+			<div>
+				<p>Se inscreva</p>
 			</div>
 		</header>
 	);
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,23 @@
+import { Link } from "@tanstack/react-router";
+import styles from "./navbar.module.css";
+
 export function Navbar(): JSX.Element {
-  return (
-    <nav>
-      <ul>
-        <li>Home</li>
-      </ul>
-    </nav>
-  )
+	return (
+		<nav>
+			<ul className={styles.list}>
+				<Link className={styles.navLink}>
+					<li>Home</li>
+				</Link>
+				<Link className={styles.navLink}>
+					<li>Filmes e séries</li>
+				</Link>
+				<Link className={styles.navLink}>
+					<li>Suporte</li>
+				</Link>
+				<Link className={styles.navLink} data-current="true">
+					<li>Planos</li>
+				</Link>
+			</ul>
+		</nav>
+	);
 }
