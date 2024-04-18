373aeec0944887a1743c0b7512aefca9118a21fb
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-04-18 14:33:36 -0300

    :heavy_plus_sign: (react-query & axios) add react-query and axios config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./@types/route-tree.gen";
 import "./global.css";
+import { queryClient } from "@/lib/react-query";
 
 const router = createRouter({ routeTree });
 
 export function App(): JSX.Element {
-	return <RouterProvider router={router} />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	);
 }
